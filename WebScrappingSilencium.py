11a70575acaa1183dcf52579c2863a1bf44c9909
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-10-11 13:26:35 +0200

    more products' features are saved
    new features are saved:
    category
    subcategory
    rating count
    rating score
    price per amount

--- WebScrappingSilencium.py
+++ WebScrappingSilencium.py
@@ -5,6 +5,13 @@ import pandas as pd
 import time
 
 
+def getCategories(url):
+    categories = url.split('/')
+    mainCat = categories[3]
+    subCat = categories[4]
+    return mainCat, subCat
+
+
 def loadThePage(url):
     urlProductsList = []
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -16,9 +23,10 @@ def loadThePage(url):
         products = driver.find_elements(By.CLASS_NAME, 'product')
         print("LEN: ", len(products))
 
-        # read every single image:
+        category, subcategory = getCategories(url)
+
         for product in products:
-            urlProductsList.append(readSingleProduct(product))
+            urlProductsList.append(readSingleProduct(product, category, subcategory))
     except:
         handleErrorReadingProducts()
 
@@ -29,17 +37,37 @@ def handleErrorReadingProducts():
     print("Error while reading the products!")
 
 
-def readSingleProduct(product):
+def getRatingCount(product):
+    ratingCountFull = product.find_element(By.XPATH,
+                                           './a/span[2]/div[2]/div/span').get_attribute("innerHTML")
+    # extract number between brackets for ex. '(18)' --> '18'
+    ratingCount = ratingCountFull[ratingCountFull.find("(") + 1:ratingCountFull.find(")")]
+    return ratingCount
+
+
+def readSingleProduct(product, category, subcategory):
     img = product.find_element(By.XPATH,
                                './a/span[1]/img').get_attribute('src')
     name = product.find_element(By.XPATH,
                                 './a/span[2]/span[1]').text
     price = product.find_element(By.XPATH,
                                  './a/span[2]/button/span/span[1]/span').text
+    ratingScore = product.find_element(By.XPATH,
+                                       './a/span[2]/div[2]/div/span/span').text
+    pricePerAmount = product.find_element(By.XPATH,
+                                          './a/span[2]/span[2]').get_attribute('data-csscontent')
+
+    ratingCount = getRatingCount(product)
+
     productItem = {
+        'cat': category,
+        'subCat': subcategory,
         'img': img,
         'name': name,
-        'price': price
+        'price': price,
+        'ratingScore': ratingScore,
+        'ratingCount': ratingCount,
+        'pricePerAmount': pricePerAmount
     }
     print(productItem)
     return productItem
@@ -49,20 +77,24 @@ def scrollWholePageDown(driver):
     finalHeight = driver.execute_script("return document.body.scrollHeight")
     singleScroll = 400
     currentScroll = 500
-    while currentScroll < finalHeight:
+    footnoteHeight = 1000
+    while currentScroll < finalHeight - footnoteHeight:
         driver.execute_script("window.scrollTo(0, " + str(currentScroll) + ")")
         currentScroll += singleScroll
         time.sleep(1)  # wait for the images to load --> links will be ready (SRCs of the images)
 
 
 if __name__ == '__main__':
+
+    # list of pages to visit and get products from
+    # the webpage cannot contain adds between the products
     urlList = [
+        'https://www.obi.pl/tynk-zaprawa-i-cement/stal-budowlana-i-kraty-budowlane/c/174'
         'https://www.obi.pl/materialy-budowlane/akcesoria-budowlane/c/176',
         'https://www.obi.pl/materialy-budowlane/akcesoria-budowlane/c/176?page=2',
         'https://www.obi.pl/materialy-budowlane/odprowadzenia-wody-i-drenaz/c/284',
         'https://www.obi.pl/tynk-zaprawa-i-cement/tynki/c/620',
         'https://www.obi.pl/tynk-zaprawa-i-cement/cement-i-wapno/c/828',
-        'https://www.obi.pl/tynk-zaprawa-i-cement/stal-budowlana-i-kraty-budowlane/c/174',
         'https://www.obi.pl/akcesoria-do-plytek/silikony-i-akryle/c/317',
         'https://www.obi.pl/akcesoria-do-plytek/silikony-i-akryle/c/317?page=2',
         'https://www.obi.pl/akcesoria-do-plytek/silikony-i-akryle/c/317?page=3',
@@ -80,6 +112,3 @@ if __name__ == '__main__':
     df.to_csv('OBI_products_1.csv', index=False, encoding='utf-8')
 
     print(df)
-
-
-
