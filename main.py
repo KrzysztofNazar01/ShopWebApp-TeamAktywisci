b822b9af4aed41717524bd4fbd7236cd46d8e32b
Krzysztof Nazar <s184698@student.pg.edu.pl>
2022-11-27 16:54:34 +0100

    prices fixed, delimiters added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import locale
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
+import random
 
 
 def loadThePage(url):
@@ -55,7 +58,7 @@ def scrollWholePageDown(driver):
         time.sleep(1)  # wait for the images to load --> links will be ready (SRCs of the images)
 
 
-if __name__ == '__main__':
+def getDataUsingWebScrapping():
     urlList = [
         'https://www.obi.pl/materialy-budowlane/akcesoria-budowlane/c/176',
         'https://www.obi.pl/materialy-budowlane/akcesoria-budowlane/c/176?page=2',
@@ -68,18 +71,107 @@ if __name__ == '__main__':
         'https://www.obi.pl/akcesoria-do-plytek/silikony-i-akryle/c/317?page=3',
         'https://www.obi.pl/akcesoria-do-plytek/silikony-i-akryle/c/317?page=4'
     ]
-
     productsList = []
-
     for url in urlList:
         newList = loadThePage(url)
         productsList += newList
-
     df = pd.DataFrame(productsList)
-
     df.to_csv('OBI_products_1.csv', index=False, encoding='utf-8')
-
     print(df)
 
 
+def extractPriceTag(row):
+    if 'zł / szt.' in row['price_brutto_zl']:
+        # row['price_brutto_zl'] = row['price_brutto_zl'].map(lambda x: x.rstrip('zł / szt.'))
+        return 'zł / szt.'
+    elif 'zł/mb' in row['price_brutto_zl']:
+        # row['price_brutto_zl'] = row['price_brutto_zl'].map(lambda x: x.rstrip('zł/mb'))
+        return 'zł/mb'
+    elif 'zł / mb' in row['price_brutto_zl']:
+        # row['price_brutto_zl'] = row['price_brutto_zl'].map(lambda x: x.rstrip('zł/mb'))
+        return 'zł / mb'
+    elif 'zł/' in row['price_brutto_zl']:
+        # row['price_brutto_zl'] = row['price_brutto_zl'].map(lambda x: x.rstrip('zł/'))
+        return 'zł/'
+    elif 'zł /' in row['price_brutto_zl']:
+        # row['price_brutto_zl'] = row['price_brutto_zl'].map(lambda x: x.rstrip('zł /'))
+        return 'zł /'
+    else:
+        # row['price_brutto_zl'] = row['price_brutto_zl'].map(lambda x: x.rstrip('zł /'))
+        return 'zł'
 
+
+def addSize(row):
+    availableWeights = [i for i in range(1, 10)]
+    minOptions = 2
+    maxOptions = 3
+    numberOfOptions = random.randint(minOptions, maxOptions)
+    options = random.sample(availableWeights, numberOfOptions)
+    options.sort()
+    options = [str(x) for x in options]
+
+    sizes = "^".join(options)
+
+    return sizes
+
+
+def addSizeColumn(df):
+    df['size'] = df.apply(lambda row: addSize(row), axis=1)
+
+    return df
+
+
+def adjustCSVfile(filename):
+    df = pd.read_csv(filename, index_col=False)
+
+    # rename brutto price column
+    df = df.rename(columns={'price': 'price_brutto_zl'})
+    print(df.head())
+    print(df.info())
+
+    # get priceTag
+    df['price_tag'] = df.apply(lambda row: extractPriceTag(row), axis=1)
+
+    # remove priceTag from each column
+    df['price_brutto_float'] = df['price_brutto_zl'].map(lambda x: x.rstrip('zł'))
+    df['price_brutto_float'] = df['price_brutto_float'].map(lambda x: x.rstrip('zł / szt.'))
+    df['price_brutto_float'] = df['price_brutto_float'].map(lambda x: x.rstrip('zł/mb'))
+    df['price_brutto_float'] = df['price_brutto_float'].map(lambda x: x.rstrip('zł/'))
+    df['price_brutto_float'] = df['price_brutto_float'].map(lambda x: x.rstrip('zł /'))
+
+    df['price_brutto_float'] = df['price_brutto_float'].str.replace(' ', '').str.replace(',', '.')
+
+    # change format: from string into float
+    df['price_brutto_float'] = df['price_brutto_float'].astype(float)
+    print(df.head())
+    print(df.info())
+
+    # add netto price column
+    df['price_netto_float'] = df.apply(lambda row: round(row.price_brutto_float * 0.77, 2), axis=1)
+    print(df.head())
+
+    # add columns with string values and priceTags
+    df['price_brutto_string'] = df['price_brutto_float'].astype(str)
+    df['price_brutto_string'] = df.apply(lambda row: row.price_brutto_string + ' ' + row.price_tag, axis=1)
+    df['price_brutto_string'] = df['price_brutto_string'].str.replace('.', ',')
+
+    df['price_netto_string'] = df['price_netto_float'].astype(str)
+    df['price_netto_string'] = df.apply(lambda row: row.price_netto_string + ' ' + row.price_tag, axis=1)
+    df['price_netto_string'] = df['price_netto_string'].str.replace('.', ',')
+
+    df = df.drop('price_brutto_zl', axis=1)
+    # df = df.drop('price_tag', axis=1)
+
+    df = addSizeColumn(df)
+    return df
+
+
+if __name__ == '__main__':
+    # getDataUsingWebScrapping()
+    newDf = adjustCSVfile('OBI_products_1.csv')
+    newDf.to_csv('OBI_products_2.csv', index=False, encoding='utf-16')
+
+    import numpy as np
+
+    my_numpy = newDf.to_numpy()
+    np.savetxt('OBI_products_2_delimiter.csv', my_numpy, fmt='%s', delimiter='|')
